76a4086c3e896e33fac85f36a0f65dcc4a77e47d
Noziop <contact@fassih.ch>
2024-10-23 11:28:23 +0200

    chore: Update initialization files - Update app initialization - Update API initialization

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from .api import api_bp
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]}})
     app.register_blueprint(api_bp)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        return response
+    
     return app
